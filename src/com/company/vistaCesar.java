4e93b90432ffe122a039d3dface40d531731b07f
Danielmg21 <46795242+Danielmg21@users.noreply.github.com>
2019-04-30 18:08:48 +0200

    vistaCesar
    Sin terminar

---
+++ src/com/company/vistaCesar.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import javax.swing.*;
+
+public class vistaCesar {
+    private JButton btVigenere;
+    private JButton btTrans;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton btCriptar;
+    private JButton btdes;
+    private JTextField textField3;
+    private JButton btdesconectar;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
